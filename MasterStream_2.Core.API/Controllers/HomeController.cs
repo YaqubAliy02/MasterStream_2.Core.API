1bf2709f2577c5f3acbfda5746e2c585183dca23
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-07-11 03:51:56 +0500

    Add project files.

---
+++ MasterStream_2.Core.API/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MasterStream_2.Core.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+           return Ok("Welcome to MasterStream Project");
+        }
+    }
+}
