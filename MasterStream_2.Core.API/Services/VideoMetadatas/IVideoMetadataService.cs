107db7f52f619744ce1c6ecd5e47e70bf46f82a5
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-07-11 09:06:43 +0500

    FOUNDATIONS: Add Services

---
+++ MasterStream_2.Core.API/Services/VideoMetadatas/IVideoMetadataService.cs
@@ -0,0 +1,9 @@
+﻿using MasterStream_2.Core.API.Models.VideoMetadatas;
+
+namespace MasterStream_2.Core.API.Services.VideoMetadatas
+{
+    public interface IVideoMetadataService
+    {
+        ValueTask<VideoMetadata> AddVideoMetadataAsync(VideoMetadata videoMetadata);
+    }
+}
