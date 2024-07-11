920b51239e4c41ae827d5c91b3559d95ade36c15
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-07-11 05:21:54 +0500

    BROKERS: Create Storages Broker and Migrations

---
+++ MasterStream_2.Core.API/Brokers/Storages/StorageBroker.VideoMetadata.cs
@@ -0,0 +1,10 @@
+﻿using MasterStream_2.Core.API.Models.VideoMetadatas;
+using Microsoft.EntityFrameworkCore;
+
+namespace MasterStream_2.Core.API.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        private DbSet<VideoMetadata> VideoMetadatas { get; set; }
+    }
+}
---
+++ MasterStream_2.Core.API/Brokers/Storages/StorageBroker.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using STX.EFxceptions.SqlServer;
+
+namespace MasterStream_2.Core.API.Brokers.Storages
+{
+    public partial class StorageBroker : EFxceptionsContext
+    {
+        private readonly IConfiguration configuration;
+
+        public StorageBroker(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            this.Database.Migrate();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = this.configuration.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
