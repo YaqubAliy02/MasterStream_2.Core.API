74367598217a411a536561d1ecf46cc46f053251
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-07-20 06:10:14 +0500

    ShouldThrowValidationExceptionOnRetrieveByIdIsInvalidAndLogItAsync -> FAIL

--- MasterStream_2.Core.API.Tests.Unit/Services/Foundations/Videometadatas/VideoMetadataServiceTests.Logic.RetreiveById.cs
+++ MasterStream_2.Core.API.Tests.Unit/Services/Foundations/Videometadatas/VideoMetadataServiceTests.Logic.RetreiveById.cs
@@ -1,7 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿//--------------------------
+// TARTEEB LLC               
+// ALL RIGHTS RESERVED      
+//--------------------------
+
+using System;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Force.DeepCloner;
---
+++ MasterStream_2.Core.API.Tests.Unit/Services/Foundations/Videometadatas/VideoMetadataServiceTests.Validations.RetrieveById.cs
@@ -0,0 +1,61 @@
+﻿//--------------------------
+// TARTEEB LLC               
+// ALL RIGHTS RESERVED      
+//--------------------------
+
+using MasterStream_2.Core.API.Models.VideoMetadatas.Exceptions;
+using MasterStream_2.Core.API.Models.VideoMetadatas;
+using Moq;
+using System.Threading.Tasks;
+using System;
+using FluentAssertions;
+
+namespace MasterStream_2.Core.API.Tests.Unit.Services.Foundations.Videometadatas
+{
+    public partial class VideoMetadataServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRetrieveByIdIsInvalidAndLogItAsync()
+        {
+            //given
+            var invalidVideoMetadataId = Guid.Empty;
+
+            var invalidVideoMetadataException =
+                new InvalidVideoMetadataException(
+                    message: "Video metadata is invalid");
+
+            invalidVideoMetadataException.AddData(
+                key: nameof(VideoMetadata.Id),
+                values: "Id is required");
+
+            var expectedVideoMetadataValidationException =
+                new VideoMetadataValidationException(
+                    message: "Video metadata validation the error occured, fix errors and try again.",
+                    innerException: invalidVideoMetadataException);
+
+            //when
+            ValueTask<VideoMetadata> retrieveByIdVideoMetadataTask =
+                this.videoMetadataService.RetrieveVideoMetadataByIdAsync(invalidVideoMetadataId);
+
+            VideoMetadataValidationException actualVideoMetadataValidationException =
+                await Assert.ThrowsAsync<VideoMetadataValidationException>(
+                    retrieveByIdVideoMetadataTask.AsTask);
+
+            //then
+            actualVideoMetadataValidationException.Should().BeEquivalentTo(
+                expectedVideoMetadataValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedVideoMetadataValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SellectVideoMetadataByIdAsync(It.IsAny<Guid>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
